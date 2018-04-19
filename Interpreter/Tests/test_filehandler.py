a0da2a1b99434db417996d57319e10ba94ddeda0
Mosplaty <36782404+Mosplaty@users.noreply.github.com>
2018-04-19 15:53:25 +1200

    Add files via upload

--- Interpreter/Tests/test_filehandler.py
+++ Interpreter/Tests/test_filehandler.py
@@ -1,4 +1,5 @@
 from Interpreter.filehandler import FileHandler
+from Interpreter.validator import a
 from unittest import TestCase
 from os import path
 
@@ -7,10 +8,13 @@ class TestFileHandler(TestCase):
 
     def setUp(self):
         self.filehandler = FileHandler(self)
-        self.maxDiff = None
+        ######
+        self.validator = a
 
     def tearDown(self):
         self.filehandler = None
+        ######
+        self.validator.a = None
 
     def test_read_csv(self):
         """
@@ -25,4 +29,20 @@ class TestFileHandler(TestCase):
         # filehandler.file_exist()
         filehandler.set_file_type()
         result = filehandler.read()
+        self.assertEqual(expected, result)
+
+    def test_read_xlsx(self):
+        """
+        Tests reading a .xlsx data file
+        """
+        expected = {0: {"ID": "A233", "Gender": "M", "Age": "22", "Sales": "245", "BMI": "Normal", "Salary": "23",
+                        "Birthday": "24/06/1995"}}
+
+        dir = path.realpath(path.curdir)
+        data = dir + "\\filehandler_tests\\data.xlsx"
+        filehandler = FileHandler(path.normpath(data))
+        # filehandler.file_exist()
+        filehandler.set_file_type()
+        result = filehandler.read()
+        print(result)
         self.assertEqual(expected, result)
